7024591512fe77f8bd094a1e3d2793a71ba87d94
Virgil Anderson <Virgil.Anderson@gmail.com>
2018-11-16 12:10:02 -0700

    Added postfix

--- project.py
+++ project.py
@@ -14,41 +14,46 @@ session = DBSession()
 # Show All Restaurants
 @app.route('/')
 @app.route('/restaurants/')
-    def showRestaurants():
-        return "This page will show all my restaurants"
+def showRestaurants():
+    return "This page will show all my restaurants"
 
 # Create New Restaurant
 @app.route('/restaurants/new')
-    def newRestaurant():
-        return "This page will let me create a new restaurant"
+def newRestaurant():
+    return "This page will let me create a new restaurant"
 
 # Edit Restaurants
-@app.route('/restaurants/<int:restaurant_id>/edit')
-    def editRestaurant():
-        return "This page will let me edit %s" % restaurant_id
+@app.route('/restaurants/restaurant_id/edit')
+def editRestaurant():
+    return "This page will let me edit %s"
 
 # Delete Restaurants
-@app.route('/restaurants/<int:restaurant_id>/delete')
-    def deleteRestaurant():
-        return "This page will let me delete %s" % restuarant_id
+@app.route('/restaurants/restaurant_id/delete')
+def deleteRestaurant():
+    return "This page will let me delete %s"
 
 # Show Menu
-@app.route('/restaurants/<int:restaurant_id>/')
-@app.route('/restaurants/<int:restaurant_id>/menu')
-    def showMenu():
-        return "This page will show the menu for %s" % restaurant_id
+@app.route('/restaurants/restaurant_id/')
+@app.route('/restaurants/restaurant_id/menu')
+def showMenu():
+    return "This page will show the menu for %s"
 
 # Create Menu item
-@app.route('/restaurants/<int:restaurant_id>/menu/new')
-    def newMenu():
-        return "This page will let me create a new menu item"
+@app.route('/restaurants/restaurant_id/menu/new')
+def newMenu():
+    return "This page will let me create a new menu item"
 
 # Edit Menu items
-@app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_id>/edit')
-    def editMenu():
-        return "This page will let me edit menu item %s" % menu_id
+@app.route('/restaurants/restaurant_id/menu/menu_id/edit')
+def editMenu():
+    return "This page will let me edit menu item %s"
 
 # Delete Menu
-@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete')
-    def deleteMenuItem():
-        return "This page will let me delete menu item %s" % menu_id
+@app.route('/restaurants/restaurant_id/menu_id/delete')
+def deleteMenuItem():
+    return "This page will let me delete menu item %s"
+
+if __name__ == '__main__':
+    app.secret_key = 'super_secret_key'
+    app.debug = True
+    app.run(host = '0.0.0.0', port = 5001)
