514334845dc450a3121834a38eb6eab1c8cf8a4a
Virgil Anderson <Virgil.Anderson@gmail.com>
2018-11-18 15:26:27 -0700

    updated the edit function

--- project.py
+++ project.py
@@ -93,24 +93,12 @@ def editMenuItem(restaurant_id, menu_id):
     else:
         return render_template('editMenuItem.html', restaurant_id = restaurant_id, menu_id = menu_id, item = editedItem)
 
-# Delete Menu
+# Delete Menu Item
 @app.route('/restaurants/restaurant_id/menu_id/delete')
 def deleteMenuItem():
     #return "This page will let me delete menu item %s"
 
     return render_template('deleteMenuItem.html', restaurant = restaurant, item = item)
-
-#Fake Restaurants
-#restaurant = {'name': 'The CRUDdy Crab', 'id': '1'}
-
-#restaurants = [{'name': 'The CRUDdy Crab', 'id': '1'}, {'name':'Blue Burgers', 'id':'2'},{'name':'Taco Hut', 'id':'3'}]
-
-
-#Fake Menu Items
-#items = [ {'name':'Cheese Pizza', 'description':'made with fresh cheese', 'price':'$5.99','course' :'Entree', 'id':'1'}, {'name':'Chocolate Cake','description':'made with Dutch Chocolate', 'price':'$3.99', 'course':'Dessert','id':'2'},{'name':'Caesar Salad', 'description':'with fresh organic vegetables','price':'$5.99', 'course':'Entree','id':'3'},{'name':'Iced Tea', 'description':'with lemon','price':'$.99', 'course':'Beverage','id':'4'},{'name':'Spinach Dip', 'description':'creamy dip with fresh spinach','price':'$1.99', 'course':'Appetizer','id':'5'} ]
-item =  {'name':'Cheese Pizza','description':'made with fresh cheese','price':'$5.99','course' :'Entree'}
-
-
 
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
